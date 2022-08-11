65561e973f21e22eb96ddcf1e581a882fd8ef32f
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-08-11 20:34:21 +0300

    add contacts to local storsge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from 'nanoid';
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
@@ -12,8 +12,27 @@ const App = () => {
   // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
   // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
   const [contacts, setContacts] = useState([]);
-  
   const [filter, setFilter] = useState("");
+  
+  useEffect(() => {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    
+    if (parsedContacts) {
+      setContacts(parsedContacts );
+    }
+  }, []);
+
+  useEffect(() => {
+    const localStorageContacts = localStorage.getItem("contacts");
+
+    if (contacts !== localStorageContacts) {
+      localStorage.setItem("contacts", JSON.stringify(contacts));
+    }
+  }, [contacts]);
+
+
+
   const handleAddContact = data => {
     const existingContactsNames = contacts.map(({ name }) => name.toLowerCase());
     if (existingContactsNames.includes(data.name.toLowerCase())) {
